92cc289da82f7f46896e112a7ab41a552e2780c7
Allena Zamoras <allenazamoras@gmail.com>
2018-07-05 15:56:55 +0800

    Add login functionality and MD5 hash

---
+++ PAP Application/src/pap/functionality/MD5.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pap.functionality;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ *
+ * @author Allena Denise
+ */
+public class MD5 {
+    
+    public static String hash(String password) throws NoSuchAlgorithmException{
+        MessageDigest m = MessageDigest.getInstance("MD5");
+        m.update(password.getBytes(),0,password.length());
+        
+        return (new BigInteger(1,m.digest()).toString(16));
+    }
+}
